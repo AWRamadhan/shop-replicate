e81fcffb97d89a180e8f230f11804e47e31920e7
AWRamadhan <annaswahyuramadhan01@gmail.com>
2020-05-20 20:35:51 +0700

    update

---
+++ olshop-replicate/src/Login.svelte
@@ -0,0 +1,49 @@
+<style>
+</style>
+
+<div class="wrapper">
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-sm">1 of 2</div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email" />
+            </div>
+            <div class="form-group">
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password" />
+            </div>
+            <button type="submit" class="btn btn-outline-primary align-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="feather feather-log-in">
+                <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+                <polyline points="10 17 15 12 10 7" />
+                <line x1="15" y1="12" x2="3" y2="12" />
+              </svg>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
